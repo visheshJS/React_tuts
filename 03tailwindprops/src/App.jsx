586d6a7c0eda4aab8fb89c225945c34bc8622458
visheshJS <visheshsharma_23me287@dtu.ac.in>
2025-01-08 19:48:31 +0530

    foundation done

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,12 +7,19 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
 
+  let myobj={
+    username:"vishesh",
+    rank:1
+  }
+
+  let newAR=[1,2,3];
+
   return (
     <>
      <h1 className='bg-white text-purple-500 
      p-3 rounded-lg'>Tailwind Test</h1>
-     <Card></Card>
-     <Card></Card>
+     <Card username="vishesh" btntext="click me"/>
+     <Card username="aman" />//isme button ki value nhi di to default use hojaega 
 
     </>
   )
