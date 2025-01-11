1b165e2384c5c78fc693e3cde488a6a7769c215e
visheshJS <visheshsharma_23me287@dtu.ac.in>
2025-01-11 12:20:53 +0530

    done

--- 05_pass_gen/src/App.jsx
+++ 05_pass_gen/src/App.jsx
@@ -52,7 +52,8 @@ function App() {
         />
         <button
         onClick={copyPasswordToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 
+        active:bg-blue-700 hover:bg-blue-400 '
         >copy</button>
         
     </div>
