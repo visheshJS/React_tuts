d8e8adebf375565a24098ce931177a69737bd92b
visheshJS <visheshsharma_23me287@dtu.ac.in>
2025-01-11 12:33:39 +0530

    currency converter start

---
+++ 06_currencyConv/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1 className='text-pink-400'>hello people
+
+      </h1>
+    
+    </div>
+      
+    </>
+  )
+}
+
+export default App
